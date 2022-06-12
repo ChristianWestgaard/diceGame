a505282079633a34eb810fd1a0aa5a3acf5bf61c
ChristianWestgaard <91060882+ChristianWestgaard@users.noreply.github.com>
2022-06-12 17:28:41 +0200

    Update java.js

--- java.js
+++ java.js
@@ -3,23 +3,23 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage1 = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource1 = "/diceGame/images/" + randomDiceImage1; //images/dice1.png - images/dice6.png
+var randomImageSource = "/diceGame/images/" + randomDiceImage1; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource1);
+image1.setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "/dicegame/images/" + randomDiceImage2;
+var randomImageSource = "/dicegame/images/" + randomDiceImage2;
 
 var image2 = document.querySelectorAll("img")[0];
 
-image2.setAttribute("src", randomImageSource2);
-document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource);
+
 
 
 //If player 1 wins
